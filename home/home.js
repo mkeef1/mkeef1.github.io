280c711e9286870bce09348ca5eed4035fd25dd9
Matt Keef <mkeef1@gmail.com>
2014-11-03 14:48:05 -0600

    bootstrap theme

--- home/home.js
+++ home/home.js
@@ -5,5 +5,6 @@
 
   home.controller('HomeCtrl', ['$scope', function($scope){
     $scope.title = 'Home Page';
+    $scope.projects = ['Example 1', 'Example 2', 'Example 3', 'Example 4', 'Example 5'];
   }]);
 })();
