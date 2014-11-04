8d5d2449a2f113d00879e09a30880cc398150505
Matt Keef <mkeef1@gmail.com>
2014-11-04 16:00:42 -0600

    template added

--- home/home.js
+++ home/home.js
@@ -9,5 +9,18 @@
     $scope.mobile   = ['Mobile 1', 'Mobile 2', 'Mobile 3'];
     $scope.games    = ['Game 1', 'Game 2', 'Game 3'];
     $scope.capstone = ['Cap 1', 'Cap 2', 'Cap 3'];
+
+    $scope.scrollTo = function(id){
+      //Angular Scroll
+      /*
+      $location.hash(id);
+      $anchorScroll();
+      */
+      //jQuery Smooth Scroll
+      var $root = $('html, body');
+      $root.animate({
+        scrollTop: $('#' + id).offset().top
+      }, 500);
+    };
   }]);
 })();
